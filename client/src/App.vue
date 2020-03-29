c42b13630c4dc63651e0150ad1a3ace6247a1de8
GitPolle <polleaubert@gmail.com>
2020-03-29 01:40:40 +0100

    Home CSS
    Eerste CSS gemaakt voor:
    achtergrond, midden block, slagschaduw van midden block, infoCorner (rechtsboven).

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div id="app">
-    <div v-if="isLoaded">
-      <Game v-if="gameStarted"></Game>
-      <Setup v-else-if="gameCreated"></Setup>
-      <Home v-else></Home>
+<div id="app">
+  <div class="infoCorner"></div>
+  <div class="infoCornerBottomLeft"></div>
+  <div class="homeCubeDropShadow"></div>
+  <div class="homeCube">
+    <div class="centerText">
+      <div v-if="isLoaded">
+        <Game v-if="gameStarted"></Game>
+        <Setup v-else-if="gameCreated"></Setup>
+        <Home v-else></Home>
+      </div>
+      <SpinnerLoader v-else color="#344558" />
     </div>
-    <SpinnerLoader v-else color="#54f1d2" />
   </div>
+</div>
 </template>
 
 <script>
@@ -34,11 +41,82 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #688980;
+}
+.homeCube {
+  display: block;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 75vmin;
+  height: 75vmin;
+  max-width: 460px;
+  max-height: 460px;
+  background-color: #F6D370;
+  z-index: 20;
+}
+.homeCubeDropShadow {
+  display: block;
+  background-color: #344558;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 106.6vmin;
+  max-width: 650.5px;
+  height: 200vw;
+  max-height: 1200px;
+  transform: translateX(-50%) rotate(-45deg);
+  transform-origin: center top;
+  z-index: 19;
+  
+}
+.centerText {
+  display: block;
+  background-color: transparent;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: max-content;
+}
+.infoCorner{
+  display: block;
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 6.5vw;
+  height: 6.5vw;
+  max-width: 40px;
+  max-height: 40px;
+  background-color: #344558;
+  box-shadow: -0.4vw 0.4vw #344558;
+  z-index: 19;
+}
+.infoCornerBottomLeft {
+  display: block;
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 0;
+  height: 0;
+  border-bottom: 6.6vw solid #F6D370;;
+  border-right: 6.6vw solid transparent;        
+  z-index: 20;
+}
+@media screen and (min-width: 613px) {
+  .infoCornerBottomLeft {
+      border-bottom: 40px solid #F6D370;;
+      border-right: 40px solid transparent; 
+  }
+  .infoCorner {
+      box-shadow: -2px 2px #344558;
+  }
 }
 </style>
--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button @click="newGame">Start nieuw spel</button>
+    <button class="transparentButton" @click="newGame"><p class="nieuwSpel">NIEUW SPEL</p></button>
   </div>
 </template>
 
@@ -16,5 +16,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+button.transparentButton {
+  display: block;
+  background: transparent;
+  border: none;
+  margin: 0;
+  padding: 0;
+}
+p.nieuwSpel {
+  font-family: Capone Light, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 12vmin;
+  font-weight: bold;
+  color: #344558;
+}
+@media screen and (min-width: 613px) and (min-height: 613px){
+  p.nieuwSpel {
+    font-size: 74px
+  }
+}
 </style>
