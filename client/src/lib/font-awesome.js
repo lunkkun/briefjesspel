aa3913dbbd33f9bed034c5121570951c3202d75f
Luuk Linders <luuk.linders@movements.nl>
2020-04-09 00:08:34 +0200

    Added times-circle icon

--- client/src/lib/font-awesome.js
+++ client/src/lib/font-awesome.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSpinner, faClipboard, faClipboardCheck, faWindowClose } from '@fortawesome/free-solid-svg-icons'
+import { faSpinner, faClipboard, faClipboardCheck, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faSpinner, faClipboard, faClipboardCheck, faWindowClose)
+library.add(faSpinner, faClipboard, faClipboardCheck, faTimesCircle)
 Vue.component('FontAwesomeIcon', FontAwesomeIcon)
