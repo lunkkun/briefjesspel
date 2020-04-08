5a717c4c34c23b8ad87da04a684b21fcae018370
Luuk Linders <luuk.linders@movements.nl>
2020-04-09 00:07:52 +0200

    Added window close icon

--- client/src/lib/font-awesome.js
+++ client/src/lib/font-awesome.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSpinner, faClipboard, faClipboardCheck } from '@fortawesome/free-solid-svg-icons'
+import { faSpinner, faClipboard, faClipboardCheck, faWindowClose } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faSpinner, faClipboard, faClipboardCheck)
+library.add(faSpinner, faClipboard, faClipboardCheck, faWindowClose)
 Vue.component('FontAwesomeIcon', FontAwesomeIcon)
