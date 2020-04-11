be098effcdac88dc4be60bad9efb8bdba8315351
GitPolle <polleaubert@gmail.com>
2020-04-11 14:55:28 +0200

    schaal/hoed/vergiet styles

--- client/src/pages/game/MyTurn.vue
+++ client/src/pages/game/MyTurn.vue
@@ -5,6 +5,10 @@
       <button class="centerBlock generalFont bigFont transparentButton" @click="startTurn">Start je beurt</button>
     </div>
     <div v-else-if="!isFinished">
+      <!-- Nog implementeren
+      <div class="bowl" :class="paperContainer"></div>
+      <div class="bowlShadow" :class="paperContainerShadow"></div>
+      -->
       <div v-if="entry">
         <button class="centerBlock generalFont bigFont transparentButton" @dblclick="nextEntry">
           <span>{{ entry.text }}<!-- TODO: font ({{ entry.font }}) --></span>
@@ -57,5 +61,47 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.bowl {
+  display: block;
+  background-image: url(/assets/img/hoed.png);
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 85vmin;
+  height: 85vmin;
+  max-height: 480px;
+  max-width: 480px;
+  z-index: 11;
+}
+.bowlShadow {
+  display: block;
+  background-color: rgba(0,0,0,0.3);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 75vmin;
+  height: 75vmin;
+  max-height: 460px;
+  max-width: 460px;
+  border-radius: 50%;
+  box-shadow: 0 0 5vmin 10vmin rgba(0,0,0,0.3);
+  z-index: 10;
+}
+
+.hat {
   
+}
+.hatShadow {
+  
+}
+
+.colander {
+  
+}
+.colanderShadow {
+  
+}
 </style>
