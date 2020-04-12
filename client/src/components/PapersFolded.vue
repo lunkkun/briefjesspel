84ee589736e64f8a2c9e7a28a1481e932a9fd36e
GitPolle <polleaubert@gmail.com>
2020-04-12 19:44:11 +0200

    papiertjessss

---
+++ client/src/components/PapersFolded.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>    
+    <div class="papersFolded" :class="papersQuantity"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PapersFolded',
+}
+</script>
+
+<style lang="scss" scoped>
+
+.papersFolded {
+  display: block;
+  background: url("~@/assets/img/paperFolded8.png");
+  background-size: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 65vmin;
+  height: 65vmin;
+}
+
+@media screen and (min-width: 613px) and (min-height: 613px) {
+  
+  .papers {
+    width: 400px;
+    height: 400px
+  }
+}
+</style>
---
+++ client/src/components/PapersUnfolded.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>    
+    <div class="papersUnfolded" :class="papersUnfoldedStyle"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PapersUnfolded',
+}
+</script>
+
+<style lang="scss" scoped>
+
+.papersUnfolded {
+  display: block;
+  background: url("~@/assets/img/paperUnfolded1.png");
+  background-size: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 65vmin;
+  height: 65vmin;
+}
+
+@media screen and (min-width: 613px) and (min-height: 613px) {
+  
+  .papersUnfolded {
+    width: 400px;
+    height: 400px
+  }
+}
+</style>
