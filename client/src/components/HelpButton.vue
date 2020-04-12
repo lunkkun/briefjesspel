d3eff42ff67d8517b925b00ae8e3b18cbcbf92dd
GitPolle <polleaubert@gmail.com>
2020-04-12 17:17:31 +0200

    dikke vinger proof

--- client/src/components/HelpButton.vue
+++ client/src/components/HelpButton.vue
@@ -1,7 +1,8 @@
 <template>
   <div @click="toggleHelp">
     <div class="infoCorner"></div>
-    <div class="infoCornerBottomLeft"></div>
+    <div class="infoCornerBottomLeft"></div>    
+    <div class="infoCornerBig"></div>
   </div>
 </template>
 
@@ -17,15 +18,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.infoCorner{
+  
+.infoCorner {
   display: block;
   position: fixed;
   top: 0;
   right: 0;
   width: 6.5vmin;
   height: 6.5vmin;
-  max-width: 40px;
-  max-height: 40px;
   background-color: #344558;
   box-shadow: -0.4vmin 0.4vmin 0.2vmin rgba(52,69,88,0.6);
   z-index: 19;
@@ -42,13 +42,30 @@ export default {
   z-index: 20;
   cursor: pointer;
 }
+.infoCornerBig {
+  display: block;
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 12vmin;
+  height: 12vmin;
+  background-color: transparent;
+  z-index: 19;
+}
+
 @media screen and (min-width: 613px) and (min-height: 613px) {
   .infoCorner {
+    width: 40px;
+    height: 40px;
     box-shadow: -2px 2px 1px rgba(52,69,88,0.6);
   }
   .infoCornerBottomLeft {
     border-bottom: 40px solid #F8DC8D;
     border-right: 40px solid transparent;
+  }
+  .infoCornerBig {
+    width: 75px;
+    height: 75px;
   }
 }
 </style>
