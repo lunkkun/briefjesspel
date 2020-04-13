9ad032dc683c6529e55090dfc8fff0a609b61fe9
Luuk Linders <luuk.linders@movements.nl>
2020-04-13 02:03:21 +0200

    Fixed double click for mobile

--- client/src/pages/game/ActiveTurn.vue
+++ client/src/pages/game/ActiveTurn.vue
@@ -2,7 +2,7 @@
   <div>
     <!-- TODO: paperBowl + toggleHomeCube implementeren -->
     <div v-if="entry">
-      <button class="centerBlock generalFont bigFont transparentButton" @dblclick="nextEntry()">
+      <button class="centerBlock generalFont bigFont transparentButton" @click="requestNextEntry()">
         <span>{{ entry.text }}<!-- TODO: font ({{ entry.font }}) --></span>
       </button>
     </div>
@@ -16,15 +16,32 @@ import Timer from '../../components/Timer'
 
 export default {
   name: 'ActiveTurn',
+  data() {
+    return {
+      clicked: false,
+    }
+  },
   components: {
     Timer,
   },
   computed: mapState({
     entry: state => state.game.activeEntry,
   }),
-  methods: mapActions([
-    'nextEntry',
-  ]),
+  methods: {
+    requestNextEntry() {
+      if (this.clicked) {
+        this.nextEntry()
+      } else {
+        this.clicked = true
+        setTimeout(() => {
+          this.clicked = false
+        }, 500)
+      }
+    },
+    ...mapActions([
+      'nextEntry',
+    ])
+  },
 }
 </script>
 
