18a3ec1046f49ea891c7fdd821ab24fe7f5ed0d5
GitPolle <polleaubert@gmail.com>
2020-04-21 21:46:25 +0200

    achtergrond briefje
    ipv PaperUnfolded als losse component, heb ik een div gemaakt om de "entry" heen die een uitgevouwen briefje als achtergrond heeft

--- client/src/pages/game/ActiveTurn.vue
+++ client/src/pages/game/ActiveTurn.vue
@@ -2,9 +2,11 @@
   <div>
     <!-- TODO: paperBowl + toggleHomeCube implementeren -->
     <div v-if="entry">
-      <button class="centerBlock generalFont bigFont transparentButton" @click="requestNextEntry()">
-        <span>{{ entry.text }}<!-- TODO: font ({{ entry.font }}) --></span>
-      </button>
+      <div class="paperUnfolded">
+        <button class="centerBlock generalFont bigFont transparentButton" @click="requestNextEntry()">
+          <span>{{ entry.text }}<!-- TODO: font ({{ entry.font }}) --></span>
+        </button>
+      </div>
     </div>
     <Timer></Timer>
   </div>
@@ -13,6 +15,7 @@
 <script>
 import {mapState, mapActions} from 'vuex'
 import Timer from '../../components/Timer'
+import PaperUnfolded from '../../components/PaperUnfolded'
 
 export default {
   name: 'ActiveTurn',
@@ -23,6 +26,7 @@ export default {
   },
   components: {
     Timer,
+    PaperUnfolded,
   },
   computed: mapState({
     entry: state => state.game.activeEntry,
@@ -46,5 +50,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  
+.paperUnfolded {
+  display: block;
+  background: url("~@/assets/img/paperUnfolded1.png");
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 98vmin;
+  height: 98vmin;
+  animation-name: unfold;
+  animation-duration: 1s;
+}
+@keyframes unfold {
+  from {transform: translate(-50%, -50%) scale(0);}
+  to {transform: translate(-50%, -50%) scale(1);}
+}
 
+@media screen and (min-width: 613px) and (min-height: 613px) {
+
+  .paperUnfolded {
+    width: 600px;
+    height: 420px;
+  }
+}
 </style>
