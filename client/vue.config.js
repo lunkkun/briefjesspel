533108fe69d646afc7d3883f44e5be9b9d8cd1e3
Luuk Linders <luuk.linders@movements.nl>
2020-04-26 17:27:40 +0200

    Preload all images and fonts

--- client/vue.config.js
+++ client/vue.config.js
@@ -8,6 +8,18 @@ module.exports = {
       }
     }
   },
+  chainWebpack: config => {
+    config.plugin('preload').tap(options => {
+      options[0].as = (entry) => {
+        if (/\.css$/.test(entry)) return 'style';
+        if (/\.(woff|ttf|otf)$/.test(entry)) return 'font';
+        if (/\.png$/.test(entry)) return 'image';
+        return 'script';
+      }
+      options[0].include = 'allAssets'
+      return options
+    })
+  },
   devServer: {
     proxy: {
       '/ws/': {
