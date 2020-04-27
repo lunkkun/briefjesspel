62a2ebb8ad6f649cb5b6a3f4a93fc52c982e5f3c
Luuk Linders <luuk.linders@movements.nl>
2020-04-27 16:45:36 +0200

    Better detection for Safari iOS

--- client/src/lib/helpers/copy-to-clipboard.js
+++ client/src/lib/helpers/copy-to-clipboard.js
@@ -1,38 +1,39 @@
 export default function (textToCopy) {
-  let textArea;
+  let textArea
 
-  function isOS() {
-    // can use a better detection logic here
-    return navigator.userAgent.match(/ipad|iphone/i);
+  function isIOSSafari() {
+    const iOS = /iPad|iPhone|iPod/i.test(window.navigator.platform)
+    const webkit = /WebKit/i.test(window.navigator.userAgent)
+    return iOS && webkit && !/CriOS|FxiOS|OPiOS|mercury/i.test(window.navigator.userAgent)
   }
 
   function createTextArea(text) {
-    textArea = document.createElement('textArea');
-    textArea.readOnly = false;
-    textArea.contentEditable = true;
-    textArea.value = text;
-    document.body.appendChild(textArea);
+    textArea = document.createElement('textArea')
+    textArea.readOnly = false
+    textArea.contentEditable = true
+    textArea.value = text
+    document.body.appendChild(textArea)
   }
 
   function selectText() {
-    if (isOS()) {
-      const range = document.createRange();
-      range.selectNodeContents(textArea);
-      const selection = window.getSelection();
-      selection.removeAllRanges();
-      selection.addRange(range);
-      textArea.setSelectionRange(0, 999999);
+    if (isIOSSafari()) {
+      const range = document.createRange()
+      range.selectNodeContents(textArea)
+      const selection = window.getSelection()
+      selection.removeAllRanges()
+      selection.addRange(range)
+      textArea.setSelectionRange(0, 999999)
     } else {
-      textArea.select();
+      textArea.select()
     }
   }
 
   function copyTo() {
-    document.execCommand('copy');
-    document.body.removeChild(textArea);
+    document.execCommand('copy')
+    document.body.removeChild(textArea)
   }
 
-  createTextArea(textToCopy);
-  selectText();
-  copyTo();
+  createTextArea(textToCopy)
+  selectText()
+  copyTo()
 }
