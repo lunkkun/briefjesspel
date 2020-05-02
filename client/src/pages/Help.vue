08e97dfd473c41329b86ae8897868ed95e79d04f
GitPolle <polleaubert@gmail.com>
2020-05-03 00:31:39 +0200

    previous + nextButton toegevoegd

--- client/src/pages/Help.vue
+++ client/src/pages/Help.vue
@@ -3,15 +3,23 @@
     <div class="generalFont mediumFont centerBlock">
       Met deze app kun je, in combinatie met een video groepsgesprek, het briefjesspel online spelen!
     </div>
+    <NextButton @click=""></NextButton>
+    <PreviousButton @click=""></PreviousButton>
   </div>
 </template>
 
 <script>
 import HomeCube from '../components/HomeCube'
+import NextButton from '../components/NextButton'
+import PreviousButton from '../components/PreviousButton'
 
 export default {
   name: 'Help',
-  components: {HomeCube},
+  components: {
+    HomeCube,
+    NextButton,
+    PreviousButton,
+    },
 }
 </script>
 
