80b388271aeac34d74926f4d45f9fe93b641cc65
Luuk Linders <luuk.linders@movements.nl>
2020-05-08 17:13:54 +0200

    Added help text

--- client/src/pages/Help.vue
+++ client/src/pages/Help.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-    <div class="generalFont mediumFont centerBlock">
-      <div v-if="page === 1">
+    <div class="generalFont centerBlock">
+      <div v-if="page === 1" class="mediumFont">
         Met deze app kun je, in combinatie met een video groepsgesprek, het briefjesspel online spelen!
       </div>
-      <div v-if="page === 2">
-        Hier volgt nog meer uitleg.
+      <div v-if="page === 2" class="smallFont">
+        Als je met jouw vriendengroep het briefjesspel wil spelen, zorg dan zelf voor een video groepsgesprek.
+        Het handigst is om de briefjesspel app op je mobiel te openen.<br>
+        Eén persoon - de spelleider - start een nieuw spel en krijgt een link die hij met de anderen kan delen.
+        Met die link kunnen ze zich aanmelden in het spel. De link is 3 uur geldig.
+        Als het spel is gestart kan er niemand meer worden toegevoegd.
+      </div>
+      <div v-if="page === 3" class="smallFont">
+        Voor als je het briefjesspel nog niet kent, hier kort de regels:<br>
+        Iedereen vult een aantal "briefjes" in met namen van bekende personen.
+        Vervolgens vorm je teams en kan het spel starten.<br>
+        Als je aan de beurt bent trek je een briefje en probeer je aan je teamgenoten te omschrijven welke persoon op je briefje staat.
+        Je mag hierbij geen namen gebruiken.
+      </div>
+      <div v-if="page === 4" class="smallFont">
+        Als je teamgenoten het goed hebben geraden scoren jullie een punt en trek je een volgende briefje, zo door todat je tijd om is.
+        Dan is iemand van een ander team aan de beurt. De ronde eindigt als alle briefjes op zijn.<br>
+        Het is leuk om hierna nog meer rondes te spelen met dezelfde briefjes.
+        Bijvoorbeeld een ronde uitbeelden en een (kortere) ronde waarin je maar één woord mag zeggen per briefje.
+      </div>
+      <div v-if="page === 5" class="smediumFont">
+        Natuurlijk kunnen jullie als groep zelf volledig de regels bepalen!
+        Als je andere rondes wil spelen of deze app zelfs voor een heel ander spel wil gebruiken (zoals hints) voel je vrij!<br>
+        Veel plezier!
       </div>
     </div>
     <PreviousButton v-if="page > 1" @click="page--"></PreviousButton>
-    <NextButton v-if="page < 2" @click="page++"></NextButton>
+    <NextButton v-if="page < 5" @click="page++"></NextButton>
   </div>
 </template>
 
